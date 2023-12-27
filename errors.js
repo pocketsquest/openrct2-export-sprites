ea894cf98532794f6e9280a2ef134e85d8cac752
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-12-27 15:22:37 -0800

    get list of files with errors from the console output

---
+++ errors.js
@@ -0,0 +1,336 @@
+const consoleOutput = `libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: ARRSW2
+Message: Command failed: openrct2 sprite exportalldat ARRSW2 .\openrct2-export-sprites\sprites\ARRSW2
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+Error executing command for file: ARRX
+Message: spawnSync C:\WINDOWS\system32\cmd.exe ENOBUFS
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: CSTBOAT
+Message: Command failed: openrct2 sprite exportalldat CSTBOAT .\openrct2-export-sprites\sprites\CSTBOAT
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: CTCAR
+Message: Command failed: openrct2 sprite exportalldat CTCAR .\openrct2-export-sprites\sprites\CTCAR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: GOLTR
+Message: Command failed: openrct2 sprite exportalldat GOLTR .\openrct2-export-sprites\sprites\GOLTR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: GTC
+Message: Command failed: openrct2 sprite exportalldat GTC .\openrct2-export-sprites\sprites\GTC
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: HELICAR
+Message: Command failed: openrct2 sprite exportalldat HELICAR .\openrct2-export-sprites\sprites\HELICAR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: HMCAR
+Message: Command failed: openrct2 sprite exportalldat HMCAR .\openrct2-export-sprites\sprites\HMCAR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: JSTAR1
+Message: Command failed: openrct2 sprite exportalldat JSTAR1 .\openrct2-export-sprites\sprites\JSTAR1
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: MFT
+Message: Command failed: openrct2 sprite exportalldat MFT .\openrct2-export-sprites\sprites\MFT
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: MONO2
+Message: Command failed: openrct2 sprite exportalldat MONO2 .\openrct2-export-sprites\sprites\MONO2
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: PTCT2
+Message: Command failed: openrct2 sprite exportalldat PTCT2 .\openrct2-export-sprites\sprites\PTCT2
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: PTCT2R
+Message: Command failed: openrct2 sprite exportalldat PTCT2R .\openrct2-export-sprites\sprites\PTCT2R
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: RCKC
+Message: Command failed: openrct2 sprite exportalldat RCKC .\openrct2-export-sprites\sprites\RCKC
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: RCR
+Message: Command failed: openrct2 sprite exportalldat RCR .\openrct2-export-sprites\sprites\RCR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SMC1
+Message: Command failed: openrct2 sprite exportalldat SMC1 .\openrct2-export-sprites\sprites\SMC1
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SMC2
+Message: Command failed: openrct2 sprite exportalldat SMC2 .\openrct2-export-sprites\sprites\SMC2
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SPBOAT
+Message: Command failed: openrct2 sprite exportalldat SPBOAT .\openrct2-export-sprites\sprites\SPBOAT
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SPCAR
+Message: Command failed: openrct2 sprite exportalldat SPCAR .\openrct2-export-sprites\sprites\SPCAR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: TRAM1
+Message: Command failed: openrct2 sprite exportalldat TRAM1 .\openrct2-export-sprites\sprites\TRAM1
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: TRUCK1
+Message: Command failed: openrct2 sprite exportalldat TRUCK1 .\openrct2-export-sprites\sprites\TRUCK1
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: VCR
+Message: Command failed: openrct2 sprite exportalldat VCR .\openrct2-export-sprites\sprites\VCR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: VEKVAMP
+Message: Command failed: openrct2 sprite exportalldat VEKVAMP .\openrct2-export-sprites\sprites\VEKVAMP
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WMMINE
+Message: Command failed: openrct2 sprite exportalldat WMMINE .\openrct2-export-sprites\sprites\WMMINE
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WMOUSE
+Message: Command failed: openrct2 sprite exportalldat WMOUSE .\openrct2-export-sprites\sprites\WMOUSE
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: ZLDB
+Message: Command failed: openrct2 sprite exportalldat ZLDB .\openrct2-export-sprites\sprites\ZLDB
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: ZLOG
+Message: Command failed: openrct2 sprite exportalldat ZLOG .\openrct2-export-sprites\sprites\ZLOG
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WTRCYAN
+Message: Command failed: openrct2 sprite exportalldat WTRCYAN .\openrct2-export-sprites\sprites\WTRCYAN
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WTRGREEN
+Message: Command failed: openrct2 sprite exportalldat WTRGREEN .\openrct2-export-sprites\sprites\WTRGREEN
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WTRGRN
+Message: Command failed: openrct2 sprite exportalldat WTRGRN .\openrct2-export-sprites\sprites\WTRGRN
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WTRORNG
+Message: Command failed: openrct2 sprite exportalldat WTRORNG .\openrct2-export-sprites\sprites\WTRORNG
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+Could not find the object.
+Error executing command for file: ObjectName
+Message: Command failed: openrct2 sprite exportalldat ObjectName .\openrct2-export-sprites\sprites\ObjectName
+Could not find the object.
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: FLYGBOAT
+Message: Command failed: openrct2 sprite exportalldat FLYGBOAT .\openrct2-export-sprites\sprites\FLYGBOAT
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: HOVERCAR
+Message: Command failed: openrct2 sprite exportalldat HOVERCAR .\openrct2-export-sprites\sprites\HOVERCAR
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: PEGASUSX
+Message: Command failed: openrct2 sprite exportalldat PEGASUSX .\openrct2-export-sprites\sprites\PEGASUSX
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SCHOOLBS
+Message: Command failed: openrct2 sprite exportalldat SCHOOLBS .\openrct2-export-sprites\sprites\SCHOOLBS
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: TRILOBTE
+Message: Command failed: openrct2 sprite exportalldat TRILOBTE .\openrct2-export-sprites\sprites\TRILOBTE
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: BLACKCAB
+Message: Command failed: openrct2 sprite exportalldat BLACKCAB .\openrct2-export-sprites\sprites\BLACKCAB
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: CRNVBFLY
+Message: Command failed: openrct2 sprite exportalldat CRNVBFLY .\openrct2-export-sprites\sprites\CRNVBFLY
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: CRNVFROG
+Message: Command failed: openrct2 sprite exportalldat CRNVFROG .\openrct2-export-sprites\sprites\CRNVFROG
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: CRNVLZRD
+Message: Command failed: openrct2 sprite exportalldat CRNVLZRD .\openrct2-export-sprites\sprites\CRNVLZRD
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: HUSKIE
+Message: Command failed: openrct2 sprite exportalldat HUSKIE .\openrct2-export-sprites\sprites\HUSKIE
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: LONDONBS
+Message: Command failed: openrct2 sprite exportalldat LONDONBS .\openrct2-export-sprites\sprites\LONDONBS
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: MANTARAY
+Message: Command failed: openrct2 sprite exportalldat MANTARAY .\openrct2-export-sprites\sprites\MANTARAY
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SANFTRAM
+Message: Command failed: openrct2 sprite exportalldat SANFTRAM .\openrct2-export-sprites\sprites\SANFTRAM
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: SURFBRDC
+Message: Command failed: openrct2 sprite exportalldat SURFBRDC .\openrct2-export-sprites\sprites\SURFBRDC
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export
+Error executing command for file: WHICGRUB
+Message: Command failed: openrct2 sprite exportalldat WHICGRUB .\openrct2-export-sprites\sprites\WHICGRUB
+libpng error: Invalid IHDR data
+Unable to write png: PNG ERRORCould not export`;
+
+// Initialize an array to store error messages
+const errorMessages = [];
+
+// Use a regular expression to match the error messages
+const regex = /Error executing command for file: (.+?)(\r|\n|$)/g;
+let match;
+
+while ((match = regex.exec(consoleOutput)) !== null) {
+    // match[1] contains the captured substring after "Error executing command for file:"
+    errorMessages.push(match[1]);
+}
+
+// Log the error messages array
+console.log('Error Messages:', errorMessages.sort());
