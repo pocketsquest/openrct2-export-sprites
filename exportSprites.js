b6974764bf91dce5ae872aac6d4184fc4d37e449
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-12-27 15:23:47 -0800

    update export file

--- exportSprites.js
+++ exportSprites.js
@@ -2,28 +2,32 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 // Read CSV file
-const datNames = fs.readFileSync('datNames.csv', 'utf-8').split('\n');
+const datNames = fs.readFileSync('datNames.csv', 'utf-8').split('\r\n');
 
-// Initialize array to store JSON outputs
-const jsonArray = [];
+// Read current offsets data
+const jsonData = fs.readFileSync('offsets.json', 'utf-8');
+const jsonArray = JSON.parse(jsonData);
+
+// Set the current working directory to the game's bin directory
+process.chdir('..');
 
 // Iterate over file names
 datNames.forEach(datName => {
-    // Execute command in the command prompt
-    const command = `openrct2 sprite exportalldat ${datName} sprites\\${datName}`;
-    const jsonOutput = execSync(command, { encoding: 'utf-8' });
+  try {
+      // Execute command in the command prompt
+      const command = `openrct2 sprite exportalldat ${datName} .\\openrct2-export-sprites\\sprites\\${datName}`;
+      const jsonOutput = execSync(command, { encoding: 'utf-8' });
 
-    // Parse JSON output and append to array
-    try {
-        const jsonData = JSON.parse(jsonOutput);
-        jsonArray.push(jsonData);
-    } catch (error) {
-        console.error(`Error parsing JSON for file: ${datName}`);
-    }
+      // Remove trailing comma and append JSON output to the array
+      const cleanedJsonOutput = jsonOutput.trim().replace(/,\s*$/, '');
+      jsonArray.push(...JSON.parse(`[${cleanedJsonOutput}]`));
+  } catch (error) {
+      console.error(`Error executing command for file: ${datName}\nMessage: ${error?.message}`);
+  }
 });
 
 // Save the array as a JSON file
-const outputFile = 'output.json';
+const outputFile = '.\\openrct2-export-sprites\\spriteOffsets.json';
 fs.writeFileSync(outputFile, JSON.stringify(jsonArray, null, 2));
 
 console.log('Script completed successfully.');
