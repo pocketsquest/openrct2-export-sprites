8f519451a4008a30bcd7a0f7d28e6d1010176c65
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-12-30 17:05:22 -0800

    get image data from png files

--- getImageData.js
+++ getImageData.js
@@ -10,17 +10,17 @@ function adjustPath(filePath) {
 // return height, width, and file size given a filepath
 function getImageData(filePath) {
   if (path.extname(filePath) !== '.png') {
-    throw new Error(`Only expecting png files`, filePath)
+    throw new Error(`${filePath} is not a png image`)
   }
   filePath = adjustPath(filePath); // make relative to the directory we are currently in
   try {
     const imageStats = fs.statSync(filePath);
-    const dimensions = sizeOf(filePath);
-    return {size: imageStats.size, ...dimensions}
+    const {height, width} = sizeOf(filePath);
+    return {size: imageStats.size, height, width}
     
   } catch (err) {
     if (err.code === "ENOENT") {
-      throw new Error(`File does not exist`, filePath);
+      throw new Error(`${filePath} does not exist`);
     } else {
       throw err
     }
@@ -28,6 +28,39 @@ function getImageData(filePath) {
 }
 
 // take an array of 
-function getAllImageData(imageArray) {
+function getImageDataForArray(imageArray) {
+
+  const newImageArray = imageArray.map(image => {
+    const imageData = getImageData(image.path);
+    return {...image, ...imageData}
+  });
+
+  return newImageArray;
+}
+
+// take an array of 
+function getAllImageData(inputFilePath, outputFilePath) {
+  const objectData = JSON.parse(fs.readFileSync(inputFilePath, 'utf-8'));
+
+  if (objectData[0].objectName) {
+    const newData = objectData.map(obj => {
+      const newImageArray = getImageDataForArray(obj.images);
+      return {...obj, images: newImageArray}
+    });
+    if (outputFilePath === "skip") {
+      fs.writeFileSync(inputFilePath, JSON.stringify(newData, null, 2));
+    } else {
+      fs.writeFileSync(outputFilePath, JSON.stringify(newData, null, 2));
+    }
+  } else if (objectData[0].path){
+    const newData = getImageDataForArray(obj.images);
+    if (outputFilePath === "skip") {
+      fs.writeFileSync(inputFilePath, JSON.stringify(newData, null, 2));
+    } else {
+      fs.writeFileSync(outputFilePath, JSON.stringify(newData, null, 2));
+    }
+  }
 
 }
+
+getAllImageData('objectDataTotalSorted.json', 'objectDataTotalSortedWithImageData.json');
