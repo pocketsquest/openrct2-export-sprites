9b2e2d8b1f64c9250b1572e4724f43e0d7288940
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2024-02-10 11:54:50 -0800

    trying to simplify config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,93 @@
+import { debuglog } from 'util';
+import assert from 'assert';
 import fs from 'fs';
 import os from 'os';
-import path from 'path';
+import p from 'path'; // 'path' is useful to have as a parameter option
+
+const debug = debuglog('config');
+
+/** 
+ * @typedef DirectoryData 
+ * @type {Object}
+ * @prop {Object[]} entriesToValidate
+ * @prop {string} entriesToValidate[].key
+ * @prop {string[]} entriesToValidate[].subPaths
+ * @prop {(path: string) => boolean} [entriesToValidate[].callback]
+ * @prop {string[]} searchLocations
+*/
+
+const directoryData = {
+  /** @type {DirectoryData} */
+  OPENRCT2_DOCS: {
+    entriesToValidate: [
+      {key: 'configIni', subPaths: ['config.ini']},
+      {key: 'objectDir', subPaths: ['object']}
+    ],
+    get searchLocations() {
+      // Make list of possible locations for OpenRCT2 user directory
+      const homePaths = [
+        os.homedir(),
+        os.userInfo().homedir,
+        process.env.HOMEPATH,
+        process.env.USERPROFILE,
+      ]
+
+      return combinePaths(homePaths, ['/Documents/OpenRCT2', '/OpenRCT2'])
+    }
+  },
+  /** @type {DirectoryData} */
+  OPENRCT2_INSTALL: {
+    // What to check to validate?  See if .exe exists?  Try doing a sample command prompt?
+    entriesToValidate: [
+      {key: 'openRct2exe', subPaths: ['OpenRCT2.exe']}
+    ],
+    get searchLocations() {
+      const programPaths = [
+        process.env.ProgramFiles,
+        process.env['ProgramFiles(x86)'],
+        process.env.ProgramFilesW6432
+      ].map(p.resolve);
+      return combinePaths(programPaths, ['/OpenRCT2'])
+    }
+  },
+  /** @type {DirectoryData} */
+  RCT2_INSTALL: {
+    entriesToValidate: [
+      {key: 'objData', subPaths: ['objData'], callback(path) {
+        // is objData a directory containing any .DAT files?
+        try {
+          const contents = fs.readdirSync(path,{withFileTypes: true});
+          return contents.some( (ent) => (ent.isFile() && p.extname(ent.name).toUpperCase() === '.DAT'));
+        } catch (e) {
+          // See if it's one of fs.statSync's or fs.readdir errors
+        }
+      }},
+      {key: 'rct2g1', subPaths: ['Data/g1.DAT','Assets/g1.DAT']}
+    ]
+  },
+  /** @type {DirectoryData} */
+  RCT1_INSTALL: { 
+    entriesToValidate: [
+      {key: 'csg1', subPaths: ['data/csg1.dat', 'data/csg1.1']},
+      {key: 'csg1i', subPaths: ['data/csg1i.dat']}
+    ],
+    get searchLocations() {
+      const programPaths = [
+        process.env.ProgramFiles,
+        process.env['ProgramFiles(x86)'],
+        process.env.ProgramFilesW6432
+      ].map(p.resolve);
+      const gamePaths = [
+        "GOG Galaxy/Games",
+        "Steam/steamapps/common",
+        "GOG Games",
+        "GalaxyClient/Games",
+        "Hasbro Interactive",
+      ];
+      return combinePaths(gamePaths, ['/RollerCoaster Tycoon Deluxe', '/RollerCoaster Tycoon'])
+    }
+  }
+}
 
 function  main() {
   const configFile = 'config.json';
@@ -8,71 +95,172 @@ function  main() {
   
   // read config.json
   try {
-    config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+    configJson = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+    config.OPENRCT2_DOCS = configJson.OPENRCT2_DOCS;
+    config.OPENRCT2_INSTALL = configJson.OPENRCT2_INSTALL;
+    config.RCT2_INSTALL = configJson.RCT2_INSTALL;
+    config.RCT1_INSTALL = configJson.RCT1_INSTALL;
+    config.IS_RCT_CLASSIC = configJson.IS_RCT_CLASSIC;
+    config.EXPORTS = configJson
   } catch (error) {
     console.error(`Error reading ${configFile}: ${error.message}`);
   }
+
 
 
 }
 
 /**
  * 
- * @param {*} dirPath 
+ * @param {string} [givenDirPath]
  */
-function getOpenRCT2Docs(dirPath) {
-  if (dirPath) {
-    const configIni = findEntry(dirPath, 'config.ini');
-    const objectDir = findEntry(dirPath, 'object');
+function getOpenRCT2Docs(givenDirPath) {
 
+  if (givenDirPath) {
+    try {
+     return {
+      directory: givenDirPath,
+      ...validate(givenDirPath, entriesToValidate.docsEntries)}
+      ;
+    } catch (err) {
+      // If the given path is invalid, note it to the console and continue
+      console.error(`Given path \"${givenDirPath}\" does not contain required data.`);
+      debug(`Given path \"${givenDirPath}\" does not contain required data.`, err.message)
+    }
   }
-}
-
-function findEntry(dirPath, subPaths, {isFile, isDirectory, callback}) {
-  if (subPaths.length === 0) {
-    throw new Error('Entry not found');
-  }
-
-  const currentPath = path.resolve(dirPath, subPaths[0]);
 
   
+  
+  for (const dirPath of docsPaths) {
+    try {
+      return {
+        directory: dirPath,
+        ...validate(dirPath,entriesToValidate.docsEntries)
+      };
+    } catch (err) {
+      debug(`Directory \"${dirPath}\" does not contain required data.`, err.message)
+    }
+  }
+  // Checked all likely locations and could not find User Directory
+  // Used Array.prototype.concat to add givenDirPath to the checkedArray if it was defined
+  throw new Error(`Could not find OpenRCT2 User Directory`, {checkedArray: docsPaths.concat(givenDirPath || []) })
+}
+
+/**
+ * Validates given OpenRCT2 install directory, othewise searches to find it.
+ * @param {string} [givenDirPath] 
+ * @returns {Object<string, string> | undefined}
+ */
+function getOpenRCT2Install(givenDirPath) {
+
+  if (givenDirPath) {
+    try {
+     return {
+      directory: givenDirPath,
+      ...validate(givenDirPath, entriesToValidate.openRCT2InstallEntries)
+      }
+    } catch (err) {
+      // If the given path is invalid, note it to the console and continue
+      console.error(`Given path \"${givenDirPath}\" does not contain required data.`);
+      debug(`Given path \"${givenDirPath}\" does not contain required data.`, err.message)
+    }
+  }
+
+  
+
+  for (const dirPath of installPaths) {
+    try {
+      return {
+        directory: dirPath,
+        ...validate(dirPath,entriesToValidate.openRCT2InstallEntries)
+      };
+    } catch (err) {
+      debug(`Directory \"${dirPath}\" does not contain required data.`, err.message)
+    }
+  }
+
+  // Checked all likely locations and could not find OpenRCT2 install directory
+  // Used Array.prototype.concat to add givenDirPath to the checkedArray if it was defined
+  throw new Error(`Could not find OpenRCT2 Install Directory`, {checkedArray: docsPaths.concat(givenDirPath || []) })
+}
+
+function getRCT2Install(givenDirPath,givenIsUsingClassic) {
+
+}
+
+/**
+ * Use {@link findEntry} to evaluate whether each item in entries has a valid path under the given directory path
+ * @param {string} dirPath - path to directory in which to search
+ * @param {object[]} entries - files or directories that `dirPath` must contain
+ * @param {string} entries[].key - name of the entry for identification in the returned object 
+ * @param {string[]} entries[].subPaths - possible relative paths to check
+ * @param {(path: string) => boolean} [entries[].callback] - an optional extra test a path must pass to be valid ( default: fs.existsSync)
+ * @returns {Object<string, string> | undefined}
+ */
+function validate(dirPath, entries) {
+  // if the directory path does not exist at all, terminate early
   try {
-    const stats = fs.statSync(currentPath);
-
-    if (isFile && !stats.isFile()) {
-      throw new Error(`Path ${currentPath} is not a file`);
+    fs.readdirSync(dirPath)
+  } catch (err) {
+    switch (err.code) {
+      case 'ERR_INVALID_ARG_TYPE':
+      case 'ENOENT':
+      case 'ENOTDIR':
+        debug(`Path \"${dirPath}\" does not exist as a directory`);
+        break;
+      default:
+        // Above are expected errors codes.  Logs error if it wasn't expected, in case it's not the directory that was the problem.
+        debug(`Failed to read directory \"${dirPath}\"`, err.message)
     }
+    throw new Error(`Path \"${dirPath}\" does not exist as a directory`, {cause: err})
+  }
 
-    if (isDirectory && !stats.isDirectory()) {
-      throw new Error(`Path ${currentPath} is not a directory`);
+  try {
+    /** @type {Object<string,string>} */
+    const result = {};
+    // For each entry needed in the directory, find a valid path and assign that value to `key` in the result object.
+    for (const {key, subPaths, callback = fs.existsSync} of entries) {
+      // using `assert` throws error if no valid paths found
+      result[key] = assert( 
+        subPaths.map( subPath => p.resolve(dirPath,subPath) ).find(callback),
+        Error(`No valid paths found in \"${dirPath}\" for ${key}`)
+        );
     }
-
-    if (callback && !callback(currentPath)) {
-      throw new Error(`Path ${currentPath} does not satisfy the callback condition`);
-    }
-
-    // If all conditions are met, return the absolute path
-    return currentPath;
-  } catch (error) {
-    // Catch any errors and recursively call findEntry with the remaining subPaths
-    return findEntry(dirPath, subPaths.slice(1), options);
+    return result;
+  } catch (err) {
+    debug(err.message)
+    throw err
   }
 }
 
-function validate(dirPath, ...entries) {
-  const resolvedPaths = [];
+/**
+ * Returns a new array with path.join of every combination from one of each of the given arrays.  It also eliminates duplicates or values that would throw an error for path functions.
+ * @param {...string} arrays - array of paths to join
+ * @returns 
+ */
+function combinePaths(...arrays) {
+  // Get rid of duplicates or values that throw an error for path functions
+  arrays = arrays.map(arr => {
+    return (arr.reduce( (newArr, currentPath) => {
+      try {
+        currentPath = p.normalize(currentPath);
+        if (!newArr.includes(currentPath)) {
+          newArr.push(currentPath);
+        }
+      } finally {
+        return newArr
+      }
+    } , [] ))
+  });
 
-  for (const entry of entries) {
-    const subPaths = (Array.isArray(entry) ? entry : [entry]);
-    const pathExists = findEntry(dirPath, ...subPaths);
-    if (pathExists) {
-      resolvedPaths.push(pathExists)
-    } else {
-      // if the path does not exist, terminate early and return false
-      return false;
+  return arrays.reduce( (accumulator, currentArray) => {
+    const result = []
+    for (const elem1 of accumulator) {
+        for (const elem2 of currentArray) {
+            const combinedPath = p.join(elem1, elem2);
+            result.push(combinedPath);
+        }
     }
-  }
-  // if loop completes, then each entry was validated
-  return resolvedPaths;
+    return result;
+  },[''])
 }
-
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,36 @@
+import fs from 'fs';
+import p from 'path';
+
 
 /** Characters disallowed in file names */
 const prohibitedCharacters = new RegExp(/[<>*\\:|?"/]/);
 
+/**
+ * Returns a new object containing only a subset of an object's properties.
+ * @param {Object<*,*>} obj 
+ * @param {(string | number | symbol)[]} props 
+ * @returns 
+ */
+function pick(obj,props) {
+  const subObj = {};
+  props.forEach((prop) => {
+    if (obj[prop]) {subObj[prop] = obj[prop];}
+  });
+  return subObj
+}
+
+
+function resolveCasing(path) {
+  // separate path into an array with each folder, ending with the basename
+  let {dir, base} = p.parse(p.normalize(path))
+  const pathsArray = []
+  while (base) {
+    pathsArray = [...pathsArray, base];
+    const parsedDir = p.parse(dir);
+    dir = parsedDir.dir;
+    base = parsedDir.base;
+  }
+
+  
+
+}
